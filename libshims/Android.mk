65a48a72ec927eaa0ec66ba2c0ee23feff856904
Ethan Chen <intervigil@gmail.com>
2025-07-28 20:35:09 +0200

    mozart: libshims: Add shim for Fence destructor
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -29,3 +29,9 @@ LOCAL_MODULE := libshim_memtrack
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libshim_fence.cpp
+LOCAL_MODULE := libshim_fence
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
---
+++ libshims/libshim_fence.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+extern "C" void _ZN7android5FenceD1Ev() {
+    // no-op, the explicit destructor was replaced with = default;
+}
